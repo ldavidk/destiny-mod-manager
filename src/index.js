607d0103923d2153177463fb5760f315e7856baf
Luke Kelly <49446724+ldavidk@users.noreply.github.com>
2022-08-02 11:10:45 -0230

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Keys from './keys.json';
+
+const url = "https://www.bungie.net/platform/Destiny/Manifest/InventoryItem/1274330687/";
+
+class SiteContainer extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			apiResults: "",
+		};
+	}
+
+	componentDidMount() {
+		this.fetchData();
+	}
+
+	fetchData() {
+		var myHeaders = new Headers();
+		myHeaders.append("x-api-key", Keys.apiKey);
+
+		var requestOptions = {
+		method: 'GET',
+		headers: myHeaders,
+		redirect: 'follow'
+		};
+
+		fetch(url, requestOptions)
+		.then(response => response.json())
+		.then(result => {
+			console.log(result)
+			this.setState({apiResults: result.Response.data.inventoryItem.itemName})
+			
+		})
+		.catch(error => console.log('error', error));
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>{this.state.apiResults}!</h1>
+			</div>
+		);
+	}
+}
+
+// ========================================a
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<SiteContainer />);
